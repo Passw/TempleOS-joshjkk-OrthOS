81229821e0c5236313fef125aefe0a2db4403380
Joshua Kinder <joshjkk0@gmail.com>
2023-04-22 20:12:32 +0100

    Fill white screen

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,4 +1,23 @@
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define SCREEN_DEPTH 4 /* 16 colors, each represented by a nibble */
+
+#define VIDEO_ADDRESS (unsigned char *)0xA0000
+
 int main(void) {
-    while(1) {}
-    return 1;
+    int pitch = (SCREEN_WIDTH * SCREEN_DEPTH + 7) / 8;
+    pitch = (pitch + 3) & ~3; /* round up to nearest multiple of 4 */
+
+    for (int y = 0; y < SCREEN_HEIGHT; y++) {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
+            unsigned char *address = VIDEO_ADDRESS + y * pitch + x / 2;
+
+            /* fill white screen */
+            if (x % 2 == 0)
+                *address = (*address & 0xF0) | 0x0F;
+            else
+                *address = (*address & 0x0F) | 0xF0;
+        }
+    }
+    return 0;
 }
